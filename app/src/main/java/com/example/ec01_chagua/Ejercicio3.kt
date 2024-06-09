43091097500e06f00d58c872d5db489a7a18b4d5
LuisChagua13 <lmcc124590@gmail.com>
2024-06-09 16:38:54 -0500

    Ejercicio3

--- app/src/main/java/com/example/ec01_chagua/Ejercicio3.kt
+++ app/src/main/java/com/example/ec01_chagua/Ejercicio3.kt
@@ -1,4 +1,22 @@
 package com.example.ec01_chagua
 
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
 class Ejercicio3 {
+
+    companion object {
+        fun debeRenovarDNI(anioNacimiento: Int): Boolean {
+            val anioActual = 2024
+            val edad = anioActual - anioNacimiento
+            return edad >= 18
+        }
+
+        @Composable
+        fun MostrarResultadoDNI(anioNacimiento: Int) {
+            val debeRenovarDNI = debeRenovarDNI(anioNacimiento)
+            Text(text = "Ejercicio 3: ¿Debe renovar DNI? ${if (debeRenovarDNI) "Sí" else "No"}")
+        }
+    }
+
 }
