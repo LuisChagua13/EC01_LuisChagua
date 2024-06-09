17422416e2b29a85ffa405be38d3e3173a9e64eb
LuisChagua13 <lmcc124590@gmail.com>
2024-06-09 16:35:57 -0500

    Ejercicio2

--- app/src/main/java/com/example/ec01_chagua/MainActivity.kt
+++ app/src/main/java/com/example/ec01_chagua/MainActivity.kt
@@ -27,8 +27,7 @@ class MainActivity : ComponentActivity() {
                 ) {
                     Column(modifier = Modifier.padding(16.dp)) {
                         Ejercicio1.Companion.MostrarResultadoEstacionamiento(horas = 3, minutos = 30)
-
-
+                        Ejercicio2.Companion.MostrarResultadoPromedio(nota1 = 15.0, nota2 = 18.0, nota3 = 20.0, nota4 = 17.0)
                     }
                 }
             }
@@ -47,6 +46,8 @@ private fun GreetingPreview() {
         ) {
             Column(modifier = Modifier.padding(16.dp)) {
                 Ejercicio1.Companion.MostrarResultadoEstacionamiento(horas = 3, minutos = 30)
+                Ejercicio2.Companion.MostrarResultadoPromedio(nota1 = 15.0, nota2 = 18.0, nota3 = 20.0, nota4 = 17.0)
+
             }
         }
     }
